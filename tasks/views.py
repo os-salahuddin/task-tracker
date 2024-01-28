f652ffa8e81a4a1de21211b05234cf047b886722
Md. Salahuddin <salahuddin@sharetrip.net>
2024-01-28 18:15:40 +0600

    completed to show tasks in view from database

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from .models import Todo
 
 # Create your views here.
 def tasks(request):
     template = loader.get_template('myfirst.html')
-    return HttpResponse(template.render())
+    tasks = Todo.objects.all().values()
+    return HttpResponse(template.render({'tasks': tasks}, request))
