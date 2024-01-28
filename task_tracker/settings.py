fb60a6c2fc33ecf6c33199d175fec5d8da4650e7
Md. Salahuddin <salahuddin@sharetrip.net>
2024-01-28 17:53:49 +0600

    added mysql database, template and migration files

--- task_tracker/settings.py
+++ task_tracker/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,15 @@ WSGI_APPLICATION = 'task_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'userPanel',
+        'USER': 'root',
+        'PASSWORD': 'root1234',
+        'HOST': 'localhost',  # or the IP address of your MySQL server
+        'PORT': '3306',      # the default MySQL port
+        'OPTIONS': {
+            'charset': 'utf8mb4',
+        },
     }
 }
 
--- tasks/views.py
+++ tasks/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 # Create your views here.
 def tasks(request):
-    return HttpResponse("Hello World")
+    template = loader.get_template('myfirst.html')
+    return HttpResponse(template.render())
